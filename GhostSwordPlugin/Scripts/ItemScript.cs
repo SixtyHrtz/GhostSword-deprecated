969c84b2f8ce1b24e1cd957daad0744d211f221b
SixtyHz <sixtyhz@outlook.com>
2018-07-31 17:08:37 +0300

    Мелкие правки

--- GhostSwordPlugin/Scripts/ItemScript.cs
+++ GhostSwordPlugin/Scripts/ItemScript.cs
@@ -3,9 +3,6 @@ using GhostSword.Types;
 using GhostSwordPlugin.Enums;
 using GhostSwordPlugin.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace GhostSwordPlugin
